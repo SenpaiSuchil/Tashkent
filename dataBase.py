b686e3fc5679025df03cb638dd4d2992f7438047
SenpaiSuchil <chompiram11@gmail.com>
2022-11-17 16:49:37 -0600

    reminder almost done

--- dataBase.py
+++ dataBase.py
@@ -5,9 +5,9 @@ class myDataBase():
     connector=mysql.connector.connect(
         user="senpaisuchil",
         password="1234",
-        host="127.0.0.1",
-        port=3306,
-        database="duck"
+        host="172.17.0.2", #ip del docker
+        database="tashkent",
+        port=3306
         )
     connector.autocommit=False
     cursor=connector.cursor()
@@ -16,12 +16,35 @@ class myDataBase():
         super().__init__()
 
     def insert(self, id, hour, status):
+        #insert values into the database
         sql="INSERT INTO reminders (user_id, time_stamp, reminder_status) VALUES (%s, %s, %s)"
         values=(f"{id}", f"{hour}", status)
         self.cursor.execute(sql, values)
         self.connector.commit()
 
     def get(self):
-        self.cursor.execute("SELECT user_id, HOUR(time_stamp), MINUTE(time_stamp) FROM reminders WHERE reminder_status=1")
+        #get the list of people who activated the reminder to check them in the loop task
+        self.cursor.execute("SELECT time_stamp, user_id FROM reminders WHERE reminder_status=1")
         result=self.cursor.fetchall()
+        return result
+
+    def verify(self, id):
+        #verify if the user has already an active reminder or is already in de database
+        #1.- if the user are already on the database and has an active rimender return 1
+        #2.- if the user are already on the database but the reminder is off, will update the value and activate the reminder and return 2
+        # 3.-if is not in the database will return 0 
+        self.cursor.execute(f"SELECT * FROM reminders WHERE user_id={id}")
+        result=self.cursor.fetchall()
+        if len(result)>0:
+            status=result[0][2]
+            if status==1:
+                return 1
+            if status==0:
+                sql=f"UPDATE reminders SET reminder_status=1 WHERE user_id={id}"
+                self.cursor.execute(sql)
+                self.connector.commit()
+                return 2
+        else:
+            return 0
+    def changeDate(self, newDate, id):
         pass
--- main.py
+++ main.py
@@ -10,12 +10,15 @@ nekoThreads={}
 dataBase=myDataBase()
 tashkent = myBot(command_prefix='-', intents=discord.Intents().all())
 
+
 TOKEN=DISCORD_TOKEN
 
 @tashkent.event
 async def on_ready():
     await tashkent.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="SenpaiSuchil"))
     print("Tashkent is ready!!!!!")
+    reminder.start()
+    print("reminder module activated")
     print("-----------------------------")
 
 @tashkent.event
@@ -32,7 +35,7 @@ async def on_member_remove(member):
         msg=f"Lastimosamente: {member} acaba de dejar {guild.name} :("
         await guild.system_channel.send(msg)
 
-#--------------------------------- zona de comandos -------------------------------------------
+#--------------------------------- command zone -------------------------------------------#
 
 @tashkent.command()
 async def schedule_start(ctx):
@@ -57,22 +60,37 @@ async def saludo(ctx):
 
 @tashkent.command()
 async def set_reminder(ctx):
-    time=datetime.datetime.now()
     author=ctx.message.author
-    dataBase.insert(str(author.id), f"{time}", 1 )
-    await ctx.reply("tu recordatorio se guardó correctamente!")
-    dataBase.get()
-    pass
+    verify=dataBase.verify(author.id)
+    if verify==0:
+        #if is not in the database
+        time=datetime.datetime.now()
+        dataBase.insert(str(author.id), f"{time}", 1 )
+        await ctx.reply("tu recordatorio se guardó correctamente!")
+    if verify==1:
+        #if the user is already in the database with an active reminder
+        await ctx.reply("Ya tienes un recordatorio activo!")
+    if verify==2:
+        #if the user is already in the database but not with an active reminder
+        await ctx.reply("Recordatorio reactivado!")
 
 
 @tashkent.command()
 async def get(ctx):
     dataBase.get()
 
-    
+
+
+#--------------------------------- loops zone -------------------------------------------#
 @tasks.loop(minutes=1)
 async def reminder():
-    pass
+    list=dataBase.get()
+    time=datetime.datetime.now()
+    for i in list:
+        date=datetime.datetime.strptime(str(i[0]), "%Y-%m-%d %H:%M:%S")
+        delta=datetime.timedelta(minutes=1)
+        
+
 
 
 tashkent.run(TOKEN)
